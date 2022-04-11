bea1c7345f25134b675d812701476c0ad151fb8a
Snade07 <99168242+Snade07@users.noreply.github.com>
2022-04-11 18:13:45 +0530

    Add files via upload

---
+++ Paymoney/main.java
@@ -0,0 +1,9 @@
+package Paymoney;
+
+public class main {
+    public static void main(String[] args) {
+
+        PayMoneyTransactionUI ui = new PayMoneyTransactionUI();
+        ui.init();
+    }
+}
---
+++ currencyspend/main.java
@@ -0,0 +1,10 @@
+package currencyspend;
+
+public class main {
+    public static void main(String[] args) {
+
+        TravellerCurrencySpendUI ui =
+                new TravellerCurrencySpendUI();
+        ui.init();
+    }
+}
